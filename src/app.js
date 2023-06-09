98c9a5531f459982d988be922177f85736c81923
NicoUmansky <umanskynico@gmail.com>
2023-06-09 17:46:26 -0300

    rutas lineas paradas y bondis

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ require('dotenv').config()
 // console.log('Connected to PlanetScale!')
 // connection.end()
 
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client');
+const { parse } = require('dotenv');
 const prisma = new PrismaClient()
 
 app.use(express.json());
@@ -18,6 +19,7 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
+// Rutas de usuarios
 app.get('/users', async(req, res) => {
     const users = await prisma.Usuarios.findMany();
     res.json({users});
@@ -60,4 +62,52 @@ app.delete('/user/:id', async (req, res) => {
         }
     });
     res.send({msg: "User deleted successfully"});
+});
+
+// Rutas de lineas
+
+app.get('/lineas', async(req, res) => {
+    const lineas = await prisma.Lineas.findMany(
+        {
+            include: {
+                colectivos: true,
+            }
+        }
+    );
+    
+    res.json({lineas});
+});
+
+app.get('/lineas/:id', async (req, res) => {
+    const { id } = req.params;
+    const linea = await prisma.Lineas.findUnique({
+        where: {
+            id: parseInt(id)
+        },
+        include: {
+            colectivos: true,
+            paradas: true,
+        }
+    });
+    res.json(linea);
+});
+
+// Rutas de Colectivos
+
+app.post('/colectivos/:id', async(req, res) => {
+    const { id } = req.params;
+    const parada = await prisma.Colectivo.create({
+        data: req.body,
+    });
+    res.json(parada);
+});
+
+// Rutas de Paradas
+
+app.post('/paradas/:id', async(req, res) => {
+    const { id } = req.params;
+    const parada = await prisma.Paradas.create({
+        data: req.body,
+    });
+    res.json(parada);
 });
